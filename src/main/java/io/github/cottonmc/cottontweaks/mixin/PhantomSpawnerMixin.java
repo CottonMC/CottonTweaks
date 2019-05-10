285ac9e293cd2679231bda84717745edaf3a6671
Pannoniae <prisznyak.zsombor@gmail.com>
2019-05-10 19:24:46 +0100

    - revert

--- src/main/java/io/github/cottonmc/cottontweaks/mixin/PhantomSpawnerMixin.java
+++ src/main/java/io/github/cottonmc/cottontweaks/mixin/PhantomSpawnerMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.world.World;
 import net.minecraft.world.biome.source.TheEndBiomeSource;
 import net.minecraft.world.dimension.TheEndDimension;
 import net.minecraft.world.gen.PhantomSpawner;
+import net.minecraft.world.gen.chunk.FloatingIslandsChunkGenerator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,8 +16,6 @@ public class PhantomSpawnerMixin {
 
     @Inject(method = "spawn", at = @At("HEAD"))
     public void doNotSpawn(World world_1, boolean boolean_1, boolean boolean_2, CallbackInfoReturnable<Integer> cir) {
-        if (!(world_1.dimension instanceof TheEndDimension)) {
-            cir.cancel();
-        }
+        cir.cancel();
     }
 }
